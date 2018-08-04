32c82cb6b303ee9ed65e84e7907a9104a971a352
schillerk <kyle@Kyles-MacBook-Pro-4.local>
2018-08-04 01:18:19 -0700

    ideas app

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,218 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function contains(arr, el) {
+  return arr.indexOf(el) > -1;
+}
+
+const TAGS = {
+  research: "Research",
+  public: "Public",
+  ethics: "Ethics / Accountability",
+  new: "New Areas",
+  internal: "Internal",
+  collab: "Collaboration",
+  dash: "Dashboards",
+  ux: "UX / Usability",
+  features: "Features",
+  impact: "Impact / M&E",
+  ml: "ML / AI",
+  analysis: "Analysis",
+  training: "Training, Documentation",
+  data: "Data Quality / New Data",
+  viz: "New and Improved Visualizations",
+  moonshot: "Moonshots",
+};
+
+const VIEWS = ['cards', 'list'];
+const FILTERS = ['all', 'none'];
+const MODES = ['view', 'edit'];
+
 class App extends Component {
   state = {
-    data: [],
-  };
-
-  componentDidMount() {
-    this.getData();
+    selectedTags: [],
+    view: 'cards',
+    defaultFilter: 'all',
+    search: '',
+    submit: '',
+    mode: 'view',
+    ideaList: [],
   }
 
-  getData = () => {
-    fetch('/api/data')
+  componentDidMount() {
+    fetch('/api/ideas')
       .then(res => res.json())
-      .then(data => this.setState({ data }));
+      .then(out => {
+        this.setState({
+          ideaList: out.map(datum => {
+            // This is dumb, basically something stupid happened when
+            // I switched from flask to node
+            if (JSON.parse(datum).label) {
+              return JSON.parse(datum);
+            } else {
+              return JSON.parse(JSON.parse(datum));
+            }
+          })
+        });
+      });
+  }
+
+  handleSubmit() {
+    const newIdea = {
+      label: this.state.submit.replace('/', '|'),
+      tags: this.state.selectedTags,
+    };
+
+    fetch(`/api/submit/${JSON.stringify(newIdea)}`);
+
+    this.setState({
+      selectedTags: [],
+      submit: '',
+      ideaList: [...this.state.ideaList, newIdea],
+    });
+  }
+
+  renderOptions(list, stateField) {
+    return list.map(el => {
+      const selectedClass = this.state[stateField] ===  el ? 'selected' : '';
+      return (
+        <div
+          key={el}
+          className={`${selectedClass} filter`}
+          onClick={() => this.setState({ [stateField]: el })}
+        >
+          {el}
+        </div>
+      );
+    });
+  }
+
+  handleClick(value) {
+    if (contains(this.state.selectedTags, value)) {
+      this.setState({ selectedTags: this.state.selectedTags.filter(tag => tag !== value) });
+    }
+    else {
+      this.setState({ selectedTags: [...this.state.selectedTags, value] });
+    }
+  }
+
+  renderTagButtons() {
+    return Object.keys(TAGS).map(tag => {
+      const selectedClass = contains(this.state.selectedTags, tag) ? 'selected' : '';
+      const colorClass = this.state.view === 'list' ? tag : '';
+      return (
+        <div
+          className={`filter ${selectedClass} ${colorClass} ${tag}`}
+          key={tag}
+          onClick={this.handleClick.bind(this, tag)}
+        >
+          {TAGS[tag]}
+        </div>
+      );
+    })
+  }
+
+  renderTags(ideaTags) {
+    const tagEls = ideaTags.map(tag => (
+      <span className={`${this.state.view}-tag ${tag}`} key={tag} data-content={TAGS[tag]}>
+        {this.state.view === 'cards' ? TAGS[tag] : ''}
+      </span>
+    ));
+    return (
+      <div className={`${this.state.view}-tags-wrapper`}>
+        {tagEls}
+      </div>
+    );
+  }
+
+  getSearchResults() {
+    return this.state.ideaList.filter(idea => {
+      return idea.label.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1;
+    });
+  }
+
+  renderIdeas() {
+    return this.getSearchResults().map(idea => {
+      if ((this.state.defaultFilter === 'none' && idea.tags.filter(tag => contains(this.state.selectedTags, tag)).length > 0)
+        || (this.state.defaultFilter === 'all' && idea.tags.filter(tag => contains(this.state.selectedTags, tag)).length < 1)) {
+        return (
+          <div key={idea.label} className={`${this.state.view}-idea`}>
+            {idea.label}
+            {this.renderTags(idea.tags)}
+          </div>
+        );
+      }
+      return null;
+    });
+  }
+
+  handleChange(field, e) {
+    this.setState({
+      [field]: e.target.value,
+      selectedTags: [],
+    });
+  }
+
+renderInput(stateField) {
+    return (
+      <input
+        className="search"
+        value={this.state[stateField]}
+        onChange={this.handleChange.bind(this, stateField)}
+      />
+    );
+  }
+
+  maybeRenderViewMode() {
+    return this.state.mode == 'view' && (
+      <span>
+        <div className="search-wrapper">
+          Search:
+          {this.renderInput('search')}
+        </div>
+        <div className="view-wrapper">
+          View As:
+          {this.renderOptions(VIEWS, 'view')}
+        </div>
+        <div className="filter-wrapper">
+          Default View:
+          {this.renderOptions(FILTERS, 'defaultFilter')}
+        </div>
+        <div className="tag-wrapper">
+          {this.renderTagButtons()}
+        </div>
+        <div className="ideas-wrapper">
+          {this.renderIdeas()}
+        </div>
+      </span>
+    );
+  }
+
+  maybeRenderEditMode() {
+    return this.state.mode == 'edit' && (
+      <span>
+        <div className="search-wrapper">
+          Submit:
+          {this.renderInput('submit')}
+        </div>
+        <div className="tag-wrapper">
+          {this.renderTagButtons()}
+        </div>
+        <div className="filter submit-button" onClick={this.handleSubmit.bind(this)}>
+          Submit
+        </div>
+      </span>
+    );
   }
 
   render() {
-    const { data } = this.state;
-
     return (
       <div className="App">
         <div>
-          {data}
-          <button
-            className="more"
-            onClick={this.getData}
-          >
-            Get More
-          </button>
+          {this.renderOptions(MODES, 'mode')}
         </div>
+        {this.maybeRenderViewMode()}
+        {this.maybeRenderEditMode()}
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,15 +1,69 @@
 const express = require('express');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Put all API endpoints under '/api'
-app.get('/api/data', (req, res) => {
-  const data = [Math.random(), Math.random()];
-  res.json(data);
+app.get('/api/ideas', (req, res) => {
+	console.log('hitting ideas endpoint');
+	const db = new sqlite3.Database('./data.db', (err) => {
+		if (err) {
+			console.log(err.message);
+		}
+		console.log('connected to database');
+	});
+
+	let sql = `SELECT * FROM ideas`;
+
+	db.all(sql, [], (err, rows) => {
+	  if (err) {
+	    throw err;
+	  }
+	  const out = []
+	  rows.forEach((row) => {
+	    out.push(row.blob);
+	  });
+	  res.json(out);
+	});
+
+	db.close((err) => {
+	  if (err) {
+	    console.error(err.message);
+	  }
+	  console.log('Close the database connection.');
+	});
+});
+
+app.get('/api/submit/:idea', (req, res) => {
+	console.log('hitting the submit endpoint');
+	const db = new sqlite3.Database('./data.db', (err) => {
+		if (err) {
+			console.log(err.message);
+		}
+		console.log('connected to database');
+	});
+
+	const newIdea = JSON.stringify(req.params.idea)
+	// console.log(req.params.idea);
+	// console.log(JSON.stringify(req.params.idea));
+	console.log(newIdea);
+	let sql = `INSERT INTO ideas VALUES(?)`;
+	db.run(sql, [newIdea], (err, rows) => {
+	  if (err) {
+	    throw err;
+	  }
+	  console.log('successful insertion');
+	});
+
+	db.close((err) => {
+	  if (err) {
+	    console.error(err.message);
+	  }
+	  console.log('Close the database connection.');
+	});
 });
 
 // The "catchall" handler: for any request that doesn't
